e95c6acda328cbda1c587d3a5f4567f8ca028547
David Abrahams <davidabrahams@sbcglobal.net>
2017-02-14 18:55:50 -0500

    okay it should work now

--- src/IMInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IMInterface extends Remote {
-    String registerUser(String u) throws RemoteException;
-    public void setClient(IMInterface c)throws RemoteException;
-    public IMInterface getClient() throws RemoteException;
-    public void sendMessage() throws RemoteException;
-}
